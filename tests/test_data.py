6d33450399cb3a752cebc7a1e9ce24ee54d7cf2d
Michael Goerz <goerz@stanford.edu>
2020-10-29 08:13:58 -0400

    Fix tests for macOS
    'md5sum' is 'md5' on macOS and has slightly different output.

--- tests/test_data.py
+++ tests/test_data.py
@@ -3,5 +3,9 @@ from cypack.answer import zen_hash
 
 
 def test_zen_hash():
-    md5sum = check_output(["md5sum", "src/cypack/data/zen.txt"]).split(maxsplit=1)[0]
+    try:
+        md5sum = check_output(["md5sum", "src/cypack/data/zen.txt"]).split(maxsplit=1)[0]
+    except FileNotFoundError:
+        # macOS
+        md5sum = check_output(["md5", "src/cypack/data/zen.txt"]).split()[-1]
     assert md5sum == zen_hash().encode("ascii")
