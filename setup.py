98340c5bf56876c85a540069af3dd5c58b9d0ebb
Michael Goerz <goerz@stanford.edu>
2020-11-03 16:30:12 -0500

    Cythonize by default

--- setup.py
+++ setup.py
@@ -1,16 +1,40 @@
 #!/usr/bin/env python3
 
 import os
-from setuptools import setup, find_packages, Extension
+from setuptools import setup, Extension
+
+CYTHONIZE = True
+# We'll cythonize by default since we can rely on Cython being installed at
+# install-time: it is a build-requirement, defined in pyproject.toml. If you
+# don't want to cythonize, but compile using the .c sources included in the
+# package source distribution, set the CYTHONIZE environment variable, e.g.
+#
+#     CYTHONIZE=0 pip install ...
+#
+if "CYTHONIZE" in os.environ:
+    print("CYTHONIZE = %s" % os.environ["CYTHONIZE"])
+    if os.environ["CYTHONIZE"] == '0':
+        CYTHONIZE = False
 
 try:
     from Cython.Build import cythonize
 except ImportError:
-    cythonize = None
+    print("WARNING: Cython not available")
+    CYTHONIZE = False
 
 
-# https://cython.readthedocs.io/en/latest/src/userguide/source_files_and_compilation.html#distributing-cython-modules
 def no_cythonize(extensions, **_ignore):
+    """Transform extensions to use packaged pre-cythonized sources.
+
+    This function replaces :func:`Cython.Build.cythonize` when running
+    ``setup.py`` from a source distribution that packages the ``.c`` files that
+    result from cythonization of ``.pyx`` files.
+
+    Adapted from https://tinyurl.com/y4aavzq5.
+    """
+    # https://tinyurl.com/y4aavzq5 ->
+    # https://cython.readthedocs.io/en/latest/src/userguide/
+    # source_files_and_compilation.html#distributing-cython-modules
     for extension in extensions:
         sources = []
         for sfile in extension.sources:
@@ -23,35 +47,37 @@ def no_cythonize(extensions, **_ignore):
     return extensions
 
 
-extensions = [
+EXTENSIONS = [
     Extension("cypack.utils", ["src/cypack/utils.pyx"]),
     Extension("cypack.answer", ["src/cypack/answer.pyx"]),
     Extension("cypack.fibonacci", ["src/cypack/fibonacci.pyx"]),
     Extension(
         "cypack.sub.wrong",
-        ["src/cypack/sub/wrong.pyx", "src/cypack/sub/helper.c"]
+        ["src/cypack/sub/wrong.pyx", "src/cypack/sub/helper.c"],
     ),
 ]
 
-CYTHONIZE = bool(int(os.getenv("CYTHONIZE", 0))) and cythonize is not None
 
 if CYTHONIZE:
-    compiler_directives = {"language_level": 3, "embedsignature": True}
-    extensions = cythonize(extensions, compiler_directives=compiler_directives)
+    EXTENSIONS = cythonize(
+        EXTENSIONS,
+        compiler_directives={"language_level": 3, "embedsignature": True},
+    )
 else:
-    extensions = no_cythonize(extensions)
+    print("WARNING: Not cythonizating")
+    EXTENSIONS = no_cythonize(EXTENSIONS)
 
 with open("requirements.txt") as fp:
-    install_requires = fp.read().strip().split("\n")
+    INSTALL_REQUIRES = fp.read().strip().split("\n")
 
 with open("requirements-dev.txt") as fp:
-    dev_requires = fp.read().strip().split("\n")
+    DEV_REQUIRES = fp.read().strip().split("\n")
 
 setup(
-    ext_modules=extensions,
-    install_requires=install_requires,
+    ext_modules=EXTENSIONS,
+    install_requires=INSTALL_REQUIRES,
     extras_require={
-        "dev": dev_requires,
-        "docs": ["sphinx", "sphinx-rtd-theme"]
+        "dev": DEV_REQUIRES,
+        "docs": ["sphinx", "sphinx-rtd-theme"],
     },
 )
